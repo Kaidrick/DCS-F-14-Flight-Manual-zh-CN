264d79cb429e03240e6e8fd5f1bd0e4cbb608c41
Kaidrick <cjt422@gmail.com>
2019-02-15 22:18:13 +0800

    nightly
    Feb. 15th, 2019

--- build/html/_static/documentation_options.js
+++ build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'alpha',
+    VERSION: 'nightly',
     LANGUAGE: 'cn',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- source/conf.py
+++ source/conf.py
@@ -22,9 +22,9 @@ copyright = '2019, Heatblur Simulations.'
 author = ''
 
 # The short X.Y version
-version = '协作预览 0.2.0 | Feb. 13th, 2019'
+version = '协作预览 0.2.1 | Feb. 15th, 2019'
 # The full version, including alpha/beta/rc tags
-release = 'alpha'
+release = 'nightly'
 
 
 # -- Localization configuration ----------------------------------------------
